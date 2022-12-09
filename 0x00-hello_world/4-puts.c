591fad9d011921af898088f80a41598cacb5cdcc
mahlet <mahi.me26@gmail.com>
2022-12-08 19:46:41 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-
-  
+#include <stdio.h>  
 /**
  * main - print a line of code using puts
  *
  * return: Always 0 (Success)
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
 
