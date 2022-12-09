84969e0dc0745cf9ef370717972856f24e3d96ff
mahlet <mahi.me26@gmail.com>
2022-12-08 16:36:57 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return: Always O (Success)
+ * return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (O);
+                 	return (0);
 }
 
