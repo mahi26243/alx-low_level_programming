f160f98fa090afb1ccbb1dfab2a51bd2f46f61c0
mahlet <mahi.me26@gmail.com>
2022-12-09 15:54:18 -0800

    alphabet game

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("last digit of %d is ", n);
-        num = n % 10;
+        num = n % (10);
      	if (num > 5)
 	{
 		printf("%d and is greater than 5\n", num);
