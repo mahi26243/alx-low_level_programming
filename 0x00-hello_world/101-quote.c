9e5e018957ab48c4c130a4c053a45cc517e33fc2
mahlet <mahi.me26@gmail.com>
2022-12-08 19:27:41 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - entry point
- * Return: 1
+ * main - print exactly and that place of art is useful
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
+
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19,\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
