1ca95d7325f212ed06af06418d61f38376940cf6
mahlet <mahi.me26@gmail.com>
2022-12-09 16:09:08 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,23 @@ int main(void)
 {
 	char (c);
 
+	char (d);
+
 	c = 'a';
+	d = 'A';
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
-	putchar(\'n)
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	{
+		putchar('\n');
+	}
 	return (0);
 
 }	
