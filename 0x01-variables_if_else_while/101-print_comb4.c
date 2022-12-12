9e70cf9abec41335675e349e0ac79bd088bb5efc
mahlet <mahi.me26@gmail.com>
2022-12-12 05:37:55 -0800

    digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,30 +12,33 @@ int main(void)
 	int (j);
 
 	for (i = 48; i < 58; i++)
-{
-	for (n = 48; n < 58; n++)
-{
-	if (n > i)
-{
-	for (j = 48; j < 58; j++)
-{
-	if (j > n)
-{
-	putchar(i);
-	putchar(n);
-	putchar(j);
-        if (i == 55 && n == 56 && j == 57)
-{
-	break;
-}
-	putchar(',');
-        putchar(' ');
-}
-}
-}
-}
-}
-	putchar('\n');
+	{
+	  for (n = 48; n < 58; n++)
+	  {
+	    if (n > i)
+	     {
+		for (j = 48; j < 58; j++)
+		  {
+		    if (j > n)
+		     {
+			putchar(i);
+			putchar(n);
+			putchar(j);
+			if (i == 55 && n == 56 && j == 57)
+			{
+			   break;
+			}
+			putchar(',');
+			putchar(' ');
+		     }
+		  }
+	     }
+	  }
+	}
+
+	{
+		putchar('\n');
+	}
 	return (0);
 }
 
