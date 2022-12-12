dd72b2dbe3ad85f71f8bf2fe1a1b22d307d451fd
mahlet <mahi.me26@gmail.com>
2022-12-11 17:29:28 -0800

    two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '9';
+
+	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
+		{
+			if (!((ones = tens) || (tens > ones)))/*eliminate repitation*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))/*addes coma and space*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
