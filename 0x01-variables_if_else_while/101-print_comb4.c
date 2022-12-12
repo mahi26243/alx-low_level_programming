ac27edcc75d074f20e0e34d31666cec36465bc77
mahlet <mahi.me26@gmail.com>
2022-12-11 22:15:17 -0800

    its hard

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,7 +23,7 @@ int main(void)
 						putchar(i);
 						putchar(n);
 						putchar(j);
-						if (i == 55 && n == 56 && j == 57)
+						if ((i == 55) && (n == 56) && (j == 57))
 						{
 							break;
 						}
