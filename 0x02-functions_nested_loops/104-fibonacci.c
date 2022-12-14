7e29f59ff85c6dd63ca3d7aa6541dbdc4d0c5204
mahlet <mahi.me26@gmail.com>
2022-12-14 14:03:40 -0800

    104fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2, separated by comma, followed by a space 
+ * main - prints the first 98 Fibonacci numbers
  * Return: Always 0.
 */
 
@@ -27,18 +27,19 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-	if (fib1_half2 + fib2_half2 > 9999999999)
-	{
+	        if (fib1_half2 + fib2_half2 > 9999999999)
+		{
 		half1 += 1;
 		half2 %= 10000000000;
-	}
-        printf("%lu%lu", half1, half2);
-        if (count != 98)
-	printf(", ");
-        fib1_half1 = fib2_half1;
-        fib1_half2 = fib2_half2;
-        fib2_half1 = half1;
-        fib2_half2 = half2;
+		}
+        
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
 	}
         printf("\n");
         return (0);
