4b2a2f86c79ed22efdf23fbab7d8378cc6abb41a
mahlet <mahi.me26@gmail.com>
2022-12-14 14:17:18 -0800

    hello

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,11 +27,11 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-	        if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
+	if (fib1_half2 + fib2_half2 > 9999999999)
+	{
+		half1 += 1;
+		half2 %= 10000000000;
+	}
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
@@ -40,7 +40,7 @@ int main(void)
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-        printf("\n");
+	printf("\n");
         return (0);
 }
 
