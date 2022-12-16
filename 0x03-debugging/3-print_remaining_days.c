a5b9729c29b94a6658e615429c3348ba8042e284
mahlet <mahi.me26@gmail.com>
2022-12-16 12:08:55 -0800

    mariyam

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 || year % 400 == 0) && !(year % 4 == 0))
 	{
-		if ((month > 1 || month < 12) && (day > 1 || day < 31))
+		if ((month >= 1 || month <= 12) && (day >= 1 || day < 31))
 		{
 			day++;
 		}
@@ -24,7 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 12 && day == 31)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
