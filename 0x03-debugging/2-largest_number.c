0534585a5085a386fd7ded6f5196c3fb7ec5f477
mahlet <mahi.me26@gmail.com>
2022-12-15 22:19:42 -0800

    0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,10 +2,10 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- *  @a: first integer
- *  @b: second integer
- *  @c: third integer
- *  Return: largest number
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
 */
 
 int largest_number(int a, int b, int c)
@@ -27,4 +27,3 @@ int largest_number(int a, int b, int c)
 
 	return (largest);
 }
-
