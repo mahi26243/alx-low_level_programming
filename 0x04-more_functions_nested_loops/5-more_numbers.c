58af472423a55a09818ce7241bdead409852add5
mahlet <mahi.me26@gmail.com>
2022-12-16 15:16:39 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
+ * _putchar only  3 times
+ * Return: always 0.
+*/
+void more_numbers(void)
+{
+	int i, ro;
+
+	for (ro = 0; ro <= 9; ro++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
