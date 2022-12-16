29834f9327d8ca8033d709c7f66e51fbbc7bd068
mahlet <mahi.me26@gmail.com>
2022-12-16 11:01:36 -0800

    chawchaw

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,11 +20,11 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 365 - day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 1 && day == 31)
+		if (month == 1 && day > 31)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
