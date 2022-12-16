730bd610634e1c7d6329ad48b4e0e64d2a4fc855
mahlet <mahi.me26@gmail.com>
2022-12-16 05:52:09 -0800

    debb

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b < a && b >= c)
+	else if (b >= c)
 	{
 		largest = b;
 	}
@@ -24,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
