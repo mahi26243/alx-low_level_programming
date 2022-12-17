d6c08d606a6e6a00f2ce2988eb5b5a9943e81cf6
mahlet <mahi.me26@gmail.com>
2022-12-16 16:20:17 -0800

    most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,13 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if ((i == 2) || (i == 4))
+		if ((i == 50) || (i == 52))
+		{
 			continue;
-
-		else
-			_putchar(i + '0');
+		}
+		_putchar(i);
 	}
-	_putchar('\n')
+	_putchar(10);
 }
