31c6b668c95721fced202f23c91e3970b2e3cff3
mahlet <mahi.me26@gmail.com>
2022-12-16 16:44:28 -0800

    moree

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - function that prints 10 times the numbers, from 0 to 14
- * _putchar only  3 times
- * Return: always 0.
+ * more_numbers - print numbers to 14
+ * 
+ * Return: returns nothing
 */
 void more_numbers(void)
 {
