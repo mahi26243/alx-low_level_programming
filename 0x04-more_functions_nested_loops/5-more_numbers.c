d476eca61add01ba6ae7a3839cf32a14686e0f11
mahlet <mahi.me26@gmail.com>
2022-12-16 16:30:45 -0800

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,17 @@ void more_numbers(void)
 {
 	int i, ro;
 
-	for (ro = 0; ro <= 9; ro++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (ro = 0; ro <= 14; ro++)
 		{
-			if (i > 9)
-				
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			if (ro > 9)
+			{	
+				_putchar((ro / 10) + '0');
+			}
+				_putchar((ro % 10) + '0');
 		}
-		_putchar('\n');
+		_putchar(10);
 	}
 }
 
