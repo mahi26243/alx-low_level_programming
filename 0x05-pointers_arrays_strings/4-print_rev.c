2d834eee68e69b68b9cd8933b47bbf79ef76915c
mahlet <mahi.me26@gmail.com>
2022-12-19 07:55:47 -0800

    rev4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev -  prints a string, in reverse, followed by a new line. 
+ * print_rev - prints a string, in reverse, followed by a new line. 
  * @s: This is the input string
  *
 */
