2935a45e239a83279512e350fec93a3a191e409a
mahlet <mahi.me26@gmail.com>
2022-12-18 22:27:43 -0800

    98 Battery st1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - pointer to an int and updates the value it points to to 98
+ * reset_to_98 - pointer to an int as parameter and updates the value it points to to 98
  *
  * Return: This is the entry
 */
