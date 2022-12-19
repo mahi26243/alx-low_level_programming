9bc1c58700690e5e76c7bb31b9849fa9b6d55e5b
mahlet <mahi.me26@gmail.com>
2022-12-19 07:51:37 -0800

    rev2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,9 @@
 
 /**
  * print_rev -  prints a string, in reverse, followed by a new line.
- * @s: This is the input string 
+ * 
+ * @s: This is the input string
+ *
 */
 void print_rev(char *s)
 {
