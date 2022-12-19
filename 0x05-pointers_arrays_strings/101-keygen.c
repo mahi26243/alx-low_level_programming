cb21eea74cf58cf598b8e2edd9cb374a8d8e0523
mahlet <mahi.me26@gmail.com>
2022-12-19 12:25:43 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdioh>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate randome valid passwords
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 -sum);
+
+	return (0);
+}
