2e0f64baa11e9fb29a7fcaff2a9c957847bc4b7a
mahlet <mahi.me26@gmail.com>
2022-12-21 12:14:55 -0800

    string g

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,13 +3,15 @@
 /**
  * _strncpy - C function that copies a string
  * @dest: buffer storing the string copy
- * @src: the source string 
- * @n: max number of byte of copied 
+ * @src: the source string
+ *
+ * @n: max number of byte of copied
+ *
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
