82931849c40ae9b065a2b574ef884fe119142196
mahlet <mahi.me26@gmail.com>
2022-12-22 15:47:36 -0800

    add

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - write your line of code here....
- * Remember:
- * - you are not allowed to use a
- *
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- * Return: always 0.
-*/
 int main(void)
 {
 	int (n);
@@ -18,6 +8,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+	/**
+	 * main - write your line of code here....
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 *
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 * Return: always 0.
+	*/
 	*(p + 5) = 98;
 	/* ...so that ths prints 98\n */
 	printf("a[2] = %d\n", a[2]);
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * infinite_add - c function that adds two numbers stored
+ * in strings to a buffer
+ * Assume that the string are never empty
+ * @n1: first number to be added
+ * @n2: second number to be added
+ * @r: store result
+ * @size_r: size of buffer
+ * Return: returns pointer to result
+ *
+*/
+
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int index, n1_len = 0, n2_len = 0;
+
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
+
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+
+	return (add_strings(n1, n2, r, --size_r));
+}
+
+/**
+ * add_strings - adds the number stored in two strings.
+ * @n1: the string containing the first number to be added
+ * @n2: The string containing the second number to be added.
+ * @r: The buffer to store the result.
+ * @r_index: The current index of the buffer
+ *
+ * Return: if r can store the sum - a pointer to the result
+ *         if r can not store the sum - 0.
+*/
+
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index++)
+	{
+		num = *(n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	return (r + r_index + 1);
+}
+
+
