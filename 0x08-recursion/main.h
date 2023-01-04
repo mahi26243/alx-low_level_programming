90a3fcd843b7b5fe61354fe5845195df58aca4ea
mahlet <mahi.me26@gmail.com>
2023-01-04 12:59:30 -0800

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 
