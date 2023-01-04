c113f6742e58567cd3df582495d86917638e04c2
mahlet <mahi.me26@gmail.com>
2023-01-04 14:47:47 -0800

    101-wildcmp.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 
