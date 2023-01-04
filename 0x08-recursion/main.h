4483f32e597f377073bcbb7f49c458a5ac01daaa
mahlet <mahi.me26@gmail.com>
2023-01-04 14:16:10 -0800

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 
