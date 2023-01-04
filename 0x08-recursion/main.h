2479553507ad2a4ef9dd95b864eda19b2565053b
mahlet <mahi.me26@gmail.com>
2023-01-04 11:41:15 -0800

    4-pow_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@
 int _putchar(char s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 
