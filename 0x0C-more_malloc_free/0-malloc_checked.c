47877d4748f5c87b775b1923f9de65adb7cfdb0b
mahlet <mahi.me26@gmail.com>
2023-01-13 12:15:58 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-malloc_checked.c -o a

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: size of memory to be allocated.
+ *
+ * Return: pointer to be allocated memory.
+ * if malloc fails, status value is equal to 98.
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
+void _puts(char *str);
+void *malloc_checked(unsigned int b);
+
+#endif
