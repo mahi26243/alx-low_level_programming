d69c1f3d8ff4a40d6e4dfa90c67580a50c4161be
mahlet <mahi.me26@gmail.com>
2023-01-26 12:52:18 -0800

    vi 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
 void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow");
-	printf(", \nI bore my house upon my back!\n");
+	printf(",\nI bore my house upon my back!\n");
 }
