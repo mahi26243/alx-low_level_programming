f021a3075c46d61a5216cc4df3ef402154d1bb8d
mahlet <mahi.me26@gmail.com>
2023-02-02 13:29:29 -0800

    ./e

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,14 +6,15 @@
  * @index: the index, starting from 0 of the bit you want to set
  *
  * Return: 1 if it worked, or -1 if an error occurred.
-*/ 
+*/
+
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))                                                                     
-		return (-1);                                                                                          
-	
-	*n &= ~(1 << index);                                                                                 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-	return (1);
+*n &= ~(1 << index);
+
+return (1);
 }
