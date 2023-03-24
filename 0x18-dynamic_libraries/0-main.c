f3d36609949204e219732921a6d57b7cf7ca3530
mahlet <mahi.me26@gmail.com>
2023-03-24 04:24:48 -0700

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -4,10 +4,10 @@
 /**
  * main - check the code
  *
- * Return: Always EXIT_SUCCESS.
-*/
+ * Return: Always EXIT SUCCESS.
+ */
 int main(void)
 {
-	printf("%d\n", _strlen("My Dyn Lib"));
-	return (EXIT_SUCCESS);
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
 }
